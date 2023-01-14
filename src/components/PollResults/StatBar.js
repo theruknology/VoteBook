8d9d10e8616671c27d61f9ac004543838df3b265
Mohammed Ruknuddin <iosrukn123@gmail.com>
2023-01-14 16:40:11 +0300

    Updating decimal point issue.

--- src/components/PollResults/StatBar.js
+++ src/components/PollResults/StatBar.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const StatBar = (props) => {
+  const simplePercent = props.percentFull.toFixed(2);
+
   return (
     <div className="bg-slate-600 p-2 flex flex-col gap-2">
       <div className="flex w-full justify-between">
         <p className="w-1/2">{props.name}</p>
         <p>{props.votes}</p>
-        <p>{props.percentFull}%</p>
+        <p>{simplePercent}%</p>
       </div>
       <div className="w-full h-2 bg-slate-400">
-        <div className={"h-full bg-slate-200"} style={{width: props.percentFull + "%"}}></div>
+        <div className={"h-full bg-slate-200"} style={{width: simplePercent + "%"}}></div>
       </div>
     </div>
   );
